651037e2354fb9b6914c42b14b804df065c1e6d5
rpatel-katalyst <rpatel@katalystadvantage.com>
2022-05-09 15:35:31 -0400

    Update index file

--- db/index.js
+++ db/index.js
@@ -7,31 +7,33 @@ class Data {
 		this.connection = connection;
 	}
     
+	// View all employees
 	viewAllEmployees() {
 		return this.connection.query(
 			`
             SELECT
-                emp.id AS ID,
-                emp.first_name AS First_Name,
-                emp.last_name AS Last_Name,
+                emp1.id AS ID,
+                emp1.first_name AS First_Name,
+                emp1.last_name AS Last_Name,
                 role.title AS Role,
                 department.name AS Department,
                 CONCAT(emp2.first_name, ' ', emp2.last_name) AS Manager,
                 role.salary AS Salary
             FROM
-                employee emp
+                employee emp1
             LEFT JOIN
-                role ON emp.role_id = role.id
+                role ON emp1.role_id = role.id
             LEFT JOIN
-                employee emp2 ON emp.manager_id = emp2.id
-		    LEFT JOIN department ON role.department_id = department.id
+                employee emp2 ON emp1.manager_id = emp2.id
+		    LEFT JOIN 
+				department ON role.department_id = department.id
 		    ORDER BY
-                emp.id;
+                emp1.id;
         `
 		);
     }
 
-	// View all employees by department and all employees details by joining employee, role and department tables.
+	// View all employees by department 
 	viewAllEmployeeByDepartments() {
 		return this.connection.query(
             `
@@ -49,14 +51,15 @@ class Data {
                 role ON emp1.role_id = role.id
             LEFT JOIN
                 employee emp2 ON emp1.manager_id = emp2.id
-            LEFT JOIN department ON role.department_id = department.id
+            LEFT JOIN 
+				department ON role.department_id = department.id
             ORDER BY
                 department.name;
             ` 
 		);
 	}
 
-	// View all employees by manager and all employees details by joining employee, role and department tables.
+	// View all employees by manager 
     viewAllEmployeesByManager(){
         return this.connection.query(
             `
@@ -73,15 +76,16 @@ class Data {
             LEFT JOIN
                 role ON emp1.role_id = role.id
             LEFT JOIN
-                employee emp2 ON e1.manager_id = emp2.id
-		    LEFT JOIN department ON role.department_id = department.id
+                employee emp2 ON emp1.manager_id = emp2.id
+		    LEFT JOIN 
+				department ON role.department_id = department.id
 		    ORDER BY
                 emp1.manager_id;
             `
         );
     }
 
-	// View all roles and role's department by joining role and department table
+	// View all roles 
 	viewAllRoles() {
 		return this.connection.query(
 			`
